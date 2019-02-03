f1003a519818f073b5c1550a4ee4861f5e0b4f69
Eric Swotinsky <eric.swotinsky@gmail.com>
2019-02-02 23:34:30 -0800

    implement filtering heroes by role

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Warrior from '../img/warrior.png';
+import Assassin from '../img/assassin.png';
+import Support from '../img/support.png';
+import Specialist from '../img/specialist.png';
+import '../css/Filters.css';
 
 function Filters(props) {
+
+  function handleFilterChange(event) {
+    event.preventDefault();
+    props.onFilterChange(event.currentTarget.alt);
+  }
+
   return (
     <div>
-      <h1>Filters Placeholder</h1>
+      <div className="role-icons">
+        <img src={Warrior} onClick={handleFilterChange} alt="Warrior" className={props.selectedRoles.includes("Warrior") ? 'selected' : ''} />
+        <img src={Assassin} onClick={handleFilterChange} alt="Assassin" className={props.selectedRoles.includes("Assassin") ? 'selected' : ''} />
+        <img src={Support} onClick={handleFilterChange} alt="Support" className={props.selectedRoles.includes("Support") ? 'selected' : ''} />
+        <img src={Specialist} onClick={handleFilterChange} alt="Specialist" className={props.selectedRoles.includes("Specialist") ? 'selected' : ''} />
+      </div>
     </div>
   );
 }
 
 Filters.propTypes = {
-  universe: PropTypes.string
+  onFilterChange: PropTypes.func
 };
 
 export default Filters;
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,14 @@ import Filters from './Filters';
 
 class Home extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       heroList: [],
+      heroListMaster: [],
+      selectedRoles: [],
       isLoading: false
     };
+    this.handleFilterChange = this.handleFilterChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,14 +20,29 @@ class Home extends Component {
     axios.get('http://hotsapi.net/api/v1/heroes')
       .then(response => this.setState({
         heroList: response.data,
+        heroListMaster: response.data,
         isLoading: false,
       }))
+  }
+
+  handleFilterChange(clickedRole) {
+    let newSelectedRoles = this.state.selectedRoles;
+    newSelectedRoles.includes(clickedRole) ? newSelectedRoles = newSelectedRoles.filter(role => role !== clickedRole) : newSelectedRoles.push(clickedRole);
+    let newHeroList = this.state.heroListMaster.filter(hero => newSelectedRoles.includes(hero.role));
+    this.setState({
+      heroList: newHeroList,
+      selectedRoles: newSelectedRoles
+    });
+    console.log(newSelectedRoles);
   }
 
   render() {
     return (
       <div>
-        <Filters />
+        <Filters
+          selectedRoles={this.state.selectedRoles}
+          onFilterChange={this.handleFilterChange}
+        />
         <HeroList
           heroList={this.state.heroList}
           isLoading={this.state.isLoading}
