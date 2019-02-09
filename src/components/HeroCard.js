7491ce0f4f08a716c3f3aa864b1d31da511d76c9
Eric Swotinsky <eric.swotinsky@gmail.com>
2019-02-08 17:19:57 -0800

    add role icons to HeroCards

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Warrior from '../img/warrior.png';
+import Assassin from '../img/assassin.png';
+import Support from '../img/support.png';
+import Specialist from '../img/specialist.png';
 import '../css/HeroCard.css';
 
 function HeroCard(props) {
+
+  const roleIcons = {
+    "Warrior": Warrior,
+    "Assassin": Assassin,
+    "Support": Support,
+    "Specialist": Specialist
+  }
 
   function handleHeroSelection() {
     props.onHeroSelection(props.name);
   }
 
   return (
-    <div className="hero-card">
-      <p>{props.name}</p>
-      <p>{props.role}</p>
+    <div className="hero-card" style={{backgroundImage: `url(${props.iconURL})`}}>
       <Link to={`/heroes/${props.name}`} onClick={handleHeroSelection}>
-        <img src = {props.iconURL} alt = {props.name} />
+        <img src={roleIcons[props.role]} className="role-icon" alt={props.name} />
+        <div className="title-plate">
+          <h4>{props.name}</h4>
+        </div>
       </Link>
     </div>
   );
